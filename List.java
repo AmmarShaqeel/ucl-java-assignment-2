79a31984e7ea27ec55a43b45e1e1b365d18ae2e0
ammar903@gmail.com <ammar903@gmail.com>
2018-03-09 12:57:02 +0000

    first commit - finished copying evrth

---
+++ List.java
@@ -0,0 +1,87 @@
+public class List{
+    //class variables 
+    protected ListNode firstNode;
+    protected ListNode lastNode;
+    protected String name;
+
+    //method to insert at front
+    public void insertAtFront (Object newData) {
+        //if list is empty
+        if (firstNode == Null) 
+            firstNode = lastNode = new ListNode(newData, null);
+        else{
+            ListNode newFirstNode = new ListNode(newData, firstNode);
+            firstNode = newFirstNode;
+        }
+    }
+    public void insertAtBack(Object newData){
+        if(firstNode == null)
+            firstNode = lastNode = new ListNode(newData, null);
+        else{
+            ListNode newLastNode = new ListNode(newData, null);
+            lastNode.setNext(newLastNode);
+            lastNode = newLastNode;
+        }
+    }
+
+    public Object removeFromFront(){
+        if (firstNode == null)
+            return null;
+
+        Object removedData = firstNode.getData();
+
+        if (firstNode == lastNode)
+            firstNode = lastNode = null;
+        else 
+            firstNode = firstNode.getNext();
+
+        return removedData;
+    }
+
+    public Object removedFromBack(){
+        if (firstNode == null)
+            return null;
+
+        Object removedData = lastNode.getData();
+
+        if (firstNode == lastNode)
+            firstNode = lastNode = null;
+        else{
+            ListNode current = firstNode;
+            
+            while (current.getNext() != lastNode)
+                current = current.getNext;
+            lastNode = current;
+            current.setNext(null);
+        }
+        
+        return removedData;
+    }
+
+    public ListNode getFirst(){
+        return firstNode;
+    }
+
+    public String toString(){
+        String output = new String();
+        ListNode current = firstNode;
+
+        output = name + ".";
+        while (current != null) {
+            output += " " + current.getData();
+            current = current.getNext();
+        }
+
+        return output;
+    }
+
+    //contructors
+    public List (String listName) {
+        firstNode = lastNode = null;
+        name = listName;
+    }
+
+    public list() {
+        this("List");
+    }
+}
---
+++ ListTest.java
@@ -0,0 +1,18 @@
+public class ListTest {
+    public static void main (String args[]) {
+            List list = new List();
+            list.insertAtFront(new Integer(-1));
+            list.insertAtBack(new Integer(2));
+            list.insertAtBack(new Integer(5));
+            list.insertAtBack(new Integer(9));
+            System.out.println(list);
+            Object removedData = list.removeFromFront();
+            System.out.println("removed data is: " + removedData);
+            removedData = list.removeFromBack();
+            // we know removedData is of class Integer so
+            // we "downcast" a superclass (Object) to a subclass (Integer)
+            Integer i = (Integer) removedData;
+            System.out.println("removed data is: " + i);
+            System.out.println(list);
+        }
+}
