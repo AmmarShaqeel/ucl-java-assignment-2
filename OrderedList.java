05545f7d58a3c7a326aa1b39b1358d7f8a009f09
ammar903@gmail.com <ammar903@gmail.com>
2018-03-15 11:41:49 +0000

    rewrote remove function to not store compare as an int

--- OrderedList.java
+++ OrderedList.java
@@ -1,10 +1,12 @@
 public abstract class OrderedList extends List {
 
+    //redefining insertAtFront to not do anything
     public void insertAtFront (Object newData) {
         System.out.println("This method is not supported");
         return;
     }
 
+    //redefining insertAtBack to not do anything
     public void insertAtBack(Object newData){
         System.out.println("This method is not supported");
         return;
@@ -15,25 +17,28 @@ public abstract class OrderedList extends List {
     public boolean insert(Object newData) {
         ListNode conductor = firstNode;
         ListNode previous = null;
-        
 
         while(conductor != null && compare(newData, conductor.getData())>= 0){
                 previous = conductor;
                 conductor = conductor.getNext();
         }
 
+        //if previous equals null, then the new element will be the first node
         if (previous == null){
             System.out.println("TK: prev = null");
             firstNode  = new ListNode(newData, conductor);
             return true;
         }
+
+        //if duplicate returns false, conductor null check is to prevent the
+        //null pointer exception
         else if (conductor != null &&  compare(newData, conductor.getData()) == 0)
         {
             System.out.println("TK: duplicate");
             return false;
         }
 
-
+        //otherwise inserts element between previous and next
         else {
             ListNode newNode = new ListNode(newData, conductor);
             previous.setNext(newNode);
@@ -46,25 +51,23 @@ public abstract class OrderedList extends List {
         ListNode conductor = firstNode;
         ListNode previous = conductor;
 
+        //checks if list is empty and returns an error
         if (firstNode == null)
         {
-            System.out.println("List is empty");
+            System.out.println("TK: Empty List");
             return false;
         }
 
-
-        int result = compare(remData, firstNode.getData());
-
-        if (result == 0) {
+        //if FirstNode is to be deleted then deletes first node
+        else if (compare(remData, firstNode.getData())== 0) {
             firstNode = firstNode.getNext();
             return true;
         }
 
+        //otherwise loops through until a match is found
         while(conductor != null){
 
-            result = compare(remData, conductor.getData());
-        
-            if (result ==  0){
+            if (compare(remData, conductor.getData())==  0){
                 previous.setNext(conductor.getNext());
                 return true;
             }
