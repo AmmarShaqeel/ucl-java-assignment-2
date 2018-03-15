9733240039a8dacb9071776c559b857391e188b7
ammar903@gmail.com <ammar903@gmail.com>
2018-03-15 10:15:30 +0000

    2nd wax

--- Menu.java
+++ Menu.java
@@ -5,38 +5,54 @@ public class Menu {
         int flag = 1;
 
         while (flag == 1) {
-            Scanner input =  new Scanner(System.in);
-            int choice  = 0;
-            System.out.println("1.Add Item 2.Delete Item 3. Print All 0.Go Back");
-            choice = input.nextInt();
+            try {
+                Scanner input =  new Scanner(System.in);
+                int choice  = 0;
+                System.out.println("1.Add Item 2.Delete Item 3. Print All 0.Go Back");
+                choice = input.nextInt();
 
-            switch(choice) {
-                case 1:
-                    System.out.println("input your data");
-                    input.nextLine();
-                    Object newData = input.nextLine();
-                    list.insert(newData);
-                    break;
+                switch(choice) {
+                    case 1:
+                        System.out.println("input your data");
+                        input.nextLine();
+                        Object newData = input.nextLine();
+                        list.insert(newData);
+                        break;
 
-                case 2:
-                    System.out.println("input the data you want removed");
-                    input.nextLine();
-                    Object remData = input.nextLine();
-                    list.remove(remData);
-                    break;
+                    case 2:
+                        System.out.println("input the data you want removed");
+                        input.nextLine();
+                        Object remData = input.nextLine();
+                        list.remove(remData);
+                        break;
 
-                case 3:
-                    System.out.println(list);
-                    break;
+                    case 3:
+                        System.out.println(list);
+                        break;
 
-                case 0:
-                    flag = 0;
-                    break;
+                    case 0:
+                        flag = 0;
+                        break;
 
-                default:
-                    System.out.println("Not an option, please try again");
-                    break;
+                    default:
+                        System.out.println("Not an option, please try again");
+                        break;
+                }
             }
+             catch (ArithmeticException e) {
+                 System.err.printf("Exception: %s \n", e);
+                 System.err.printf("Numbers should be positive integers different than zero   \n");
+             }
+
+             catch (NumberFormatException e) {
+                 System.err.printf("Exception: %s \n", e);
+                 System.err.printf("integers please \n");
+             }
+
+             catch (InputMismatchException e) {
+                 System.err.printf("Exception: %s \n", e);
+                 System.err.printf("Inputs should be integers \n");
+             }
         }
     }
         
